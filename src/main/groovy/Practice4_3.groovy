a064c096c416349c5ec557e7a3befad159289f78
yaroslav.yermilov <yermilov17@gmail.com>
2014-11-20 03:06:50 +0200

    practice 4, part 3

---
+++ src/main/groovy/Practice4_3.groovy
@@ -0,0 +1,22 @@
+import com.github.cyberwaste.muka.Index
+
+/**
+ * @author yaroslav.yermilov
+ */
+
+def index = new Index(ngram: 3, documents: (1..1).collect { "text-${it}.txt" })
+index.load()
+
+println index.terms.sort().collect { it.toString() }.join('\n')
+
+index.terms.sort().each { term ->
+    print "${term.key}".center(50)
+    (1..10).collect { "text-${it}.txt" }.each { document ->
+        if (term.value.contains(document)) {
+            print '1'
+        } else {
+            print '0'
+        }
+    }
+    println()
+}
