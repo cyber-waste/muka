c5f7eb0d38e99ed74e4078230f81b21c94b4d79d
yaroslav.yermilov <yermilov17@gmail.com>
2014-12-05 13:52:03 +0200

    lab #7

--- src/main/groovy/Practice7.groovy
+++ src/main/groovy/Practice7.groovy
@@ -1,3 +1,10 @@
+import com.github.cyberwaste.muka.ZonnedIndex
+
 /**
  * @author yaroslav.yermilov
  */
+
+def index = new ZonnedIndex(documents: (1..10).collect { "text-${it}.txt" })
+index.load()
+
+println index.terms.sort().collect { it.toString() }.join('\n')
